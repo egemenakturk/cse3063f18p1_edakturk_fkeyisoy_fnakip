ac96edace22351d5629b89386c1bc33bda1af8ec
Furkan <furkannkp@gmail.com>
2018-10-31 15:09:36 +0300

    first commit

---
+++ src/Die.java
@@ -0,0 +1,12 @@
+
+public class Die {
+   private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
