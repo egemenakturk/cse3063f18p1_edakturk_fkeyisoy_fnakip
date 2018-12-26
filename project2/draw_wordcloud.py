51fd2644d421dc32de115d5423bcc97756b3a979
Furkan <furkannkp@gmail.com>
2018-12-26 12:57:54 +0300

    word_cloud.py is added

---
+++ project2/draw_wordcloud.py
@@ -0,0 +1,12 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+
+
+def draw_tf(tf_top_50):
+    mask = np.array(Image.open('./masks/twitter_mask.png'))
+    cloud = WordCloud(background_color='white', relative_scaling=0.7, width=1366, height=768, mask=mask).generate_from_frequencies(tf_top_50)
+    plt.imshow(cloud)
+    plt.axis('off')
+    plt.savefig('./output/tf_wordcloud.pdf', format='pdf')
